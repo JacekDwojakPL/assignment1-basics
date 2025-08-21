8bd4f5a0e8cf0ee1d47c4ac8fe91cbce3f7ffdbd
JacekDwojakPL <dwojak.jacek@gmail.com>
2025-08-21 21:23:09 +0200

    small changes

--- cs336_basics/train_bpe.py
+++ cs336_basics/train_bpe.py
@@ -6,23 +6,24 @@ def train_bpe(input_path, vocab_size, special_tokens):
     vocab.update({i + len(special_tokens): bytes([i]) for i in range(256)})
 
     merges = []
-    words_counter, pairs_counter, pairs_to_word, word_to_pairs = get_word_counts(input_path, 4, special_tokens)
+    words_counter, pairs_counter, pairs_to_word, word_to_pairs = get_word_counts(input_path, 16, special_tokens)
     if vocab_size > len(vocab):
         vocab_size = vocab_size - len(vocab)
 
     for i in range(vocab_size):
         # get most frequent pair
-        most_frequent_pair = max(pairs_counter.items(), key=lambda p: (p[1], p[0]))
-        if len(most_frequent_pair) == 0:
+        if not pairs_counter:
             break
+        most_frequent_pair = max(pairs_counter.items(), key=lambda p: (p[1], p[0]))
         
         pair, pair_count = most_frequent_pair
         del pairs_counter[pair]
         # get words with that pair
         affected_words = pairs_to_word[pair].copy()
         # make new merge and vocab index
+        new_idx = 256 + len(special_tokens) + i
+        vocab[new_idx] = pair[0].encode() + pair[1].encode()
         merges.append((pair[0].encode(), pair[1].encode()))
-        vocab[256 + len(special_tokens) + i] = pair[0].encode() + pair[1].encode()
 
         for word in affected_words:
             word_count = words_counter[word]
@@ -32,6 +33,8 @@ def train_bpe(input_path, vocab_size, special_tokens):
 
             for p_remove in old_pairs:
                 pairs_counter[p_remove] -= word_count
+                if pairs_counter[p_remove] <= 0:
+                    del pairs_counter[p_remove]
                 pairs_to_word[p_remove].discard(word)
 
             for p_add in new_pairs:
@@ -40,7 +43,7 @@ def train_bpe(input_path, vocab_size, special_tokens):
             
             del word_to_pairs[word]
             word_to_pairs[new_word] = new_pairs
-            words_counter[new_word] = words_counter[word]
+            words_counter[new_word] = word_count
             del words_counter[word]
 
     return vocab, merges
