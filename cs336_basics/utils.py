9870b00bc7405a56cc2b2434a5c3497e4ce98a17
JacekDwojakPL <dwojak.jacek@gmail.com>
2025-08-25 21:18:58 +0200

    move pretokenization to parallel processing

--- cs336_basics/utils.py
+++ cs336_basics/utils.py
@@ -2,6 +2,12 @@ import multiprocessing as mp
 from collections import Counter, defaultdict
 import regex as re
 from .pretokenization_example import find_chunk_boundaries
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,  # Set minimum level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 SPLIT_PATTERN = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 BYTE_MAP = {i: bytes([i]) for i in range(256)}
@@ -42,26 +48,40 @@ def update_counters(pretokens, counters):
 
 def read_and_count_chunk(input_path, start_chunk, end_chunk, split_special_token):
     result = []
-    with open(input_path) as f:
+    counters = init_counters()
+    with open(input_path, "rb") as f:
         f.seek(start_chunk)
-        text = f.read(end_chunk - start_chunk)
+        text = f.read(end_chunk - start_chunk).decode("utf-8", errors="ignore")
         for doc in re.splititer(split_special_token, text):
             matches = re.finditer(SPLIT_PATTERN, doc)
             for m in matches:
                 result.extend([tuple(BYTE_MAP[p] for p in m.group().encode())])
-    return result
+    update_counters(result, counters)
+    logging.info(f"end processing chunk {start_chunk} {end_chunk}")
+    return counters
 
 def get_word_counts(input_path, num_chunks, special_tokens=[]):
-    counters = init_counters()
+    words_counter, pairs_counter, pairs_to_word, word_to_pairs = init_counters()
     split_special_token = "|".join([token for token in special_tokens])
-    cb = lambda pretokens: update_counters(pretokens, counters)
     f = open(input_path, "rb")
     chunks = find_chunk_boundaries(f, num_chunks, split_special_token=split_special_token.encode("utf-8"))
     f.close()
     pool = mp.Pool(mp.cpu_count())
+    result = []
+    
     for start_chunk, end_chunk in zip(chunks[:-1], chunks[1:]):
-        pool.apply_async(read_and_count_chunk, (input_path, start_chunk, end_chunk, split_special_token), callback=cb)
+        logging.info(f"start processing chunk {start_chunk} {end_chunk}")
+        result.append(pool.apply_async(read_and_count_chunk, (input_path, start_chunk, end_chunk, split_special_token)))
     pool.close()
     pool.join()
+    
+    for r in result:
+        l_words_counter, l_pairs_counter, l_pairs_to_word, l_word_to_pairs = r.get()
+        words_counter.update(l_words_counter)
+        pairs_counter.update(l_pairs_counter)
+        for pair, words in l_pairs_to_word.items():
+            pairs_to_word[pair].update(words)
+        for word, pairs in l_word_to_pairs.items():
+            word_to_pairs[word] = pairs
 
-    return counters
+    return (words_counter, pairs_counter, pairs_to_word, word_to_pairs)
