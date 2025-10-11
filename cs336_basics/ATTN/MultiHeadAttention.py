128073ebe29b1ecd129eeca1ed42ceac68a6194a
JacekDwojakPL <dwojak.jacek@gmail.com>
2025-10-11 19:29:46 +0200

    use Linear module in MHA

--- cs336_basics/ATTN/MultiHeadAttention.py
+++ cs336_basics/ATTN/MultiHeadAttention.py
@@ -1,9 +1,11 @@
 import torch
 from einops import rearrange, einsum
 from .ScaledDotProductAttention import scaled_dot_product_attention
+from .RotaryPositionEmbeddings import RotaryPositionEmbeddings
+from cs336_basics.NN import Linear
 
 class MultiHeadAttention(torch.nn.Module):
-    def __init__(self, d_model, num_heads):
+    def __init__(self, d_model, num_heads, with_rope=False, max_seq_len=None, theta=None):
         super(MultiHeadAttention, self).__init__()
         self.d_model = d_model
         self.num_heads = num_heads
@@ -11,34 +13,35 @@ class MultiHeadAttention(torch.nn.Module):
         self.d_k = self.d_q
         self.d_v = self.d_q
         self.d_o = self.d_q
-        self.Q = torch.nn.Parameter(torch.randn((self.d_model, self.d_model)))
-        self.K = torch.nn.Parameter(torch.randn((self.d_model, self.d_model)))
-        self.V = torch.nn.Parameter(torch.randn((self.d_model, self.d_model)))
-        self.O = torch.nn.Parameter(torch.randn((self.d_model, self.d_model)))
+        self.Q = Linear(self.d_model, self.d_model)
+        self.K = Linear(self.d_model, self.d_model)
+        self.V = Linear(self.d_model, self.d_model)
+        self.O = Linear(self.d_model, self.d_model)
+        self.rope = None
+        if with_rope:
+            self.rope = RotaryPositionEmbeddings(theta=theta, d_k=self.d_k, max_seq_len=max_seq_len)
    
-    def forward(self, in_features):
+    def forward(self, in_features, token_positions=None):
         seq_length = in_features.shape[1]
         
-        q = einsum(in_features, self.Q, "... embed_dim, out_dim embed_dim -> ... out_dim")
-        q = rearrange(q, "... seq_len (num_head head_dim) -> ... num_head seq_len head_dim", num_head=self.num_heads)
+        q = rearrange(self.Q(in_features), "... seq_len (num_head head_dim) -> ... num_head seq_len head_dim", num_head=self.num_heads)
+        k = rearrange(self.K(in_features), "... seq_len (num_head head_dim) -> ... num_head seq_len head_dim", num_head=self.num_heads)
+        v = rearrange(self.V(in_features), "... seq_len (num_head head_dim) -> ... num_head seq_len head_dim", num_head=self.num_heads)
         
-        k = einsum(in_features, self.K, "... embed_dim, out_dim embed_dim -> ... out_dim")
-        k = rearrange(k, "... seq_len (num_head head_dim) -> ... num_head seq_len head_dim", num_head=self.num_heads)
+        if self.rope:
+            q = self.rope.forward(q, token_positions[0])
+            k = self.rope.forward(k, token_positions[0])
         
-        v = einsum(in_features, self.V, "... embed_dim, out_dim embed_dim -> ... out_dim")
-        v = rearrange(v, "... seq_len (num_head head_dim) -> ... num_head seq_len head_dim", num_head=self.num_heads)
         
         mask = torch.tril(torch.ones((seq_length, seq_length))) == 1
         
         context = scaled_dot_product_attention(query=q, key=k, value=v, boolean_mask=mask)
         context = rearrange(context, "... num_head seq_len head_dim -> ... seq_len (num_head head_dim)")
         
-        out = einsum(context, self.O, "... seq_len embed_dim, out_dim embed_dim-> ... seq_len out_dim")
-
-        return out
+        return self.O(context)
     
     def load_state_dict(self, state_dict):
-        self.Q.data = state_dict["q_proj_weight"]
-        self.K.data = state_dict["k_proj_weight"]
-        self.V.data = state_dict["v_proj_weight"]
-        self.O.data = state_dict["o_proj_weight"]
+        self.Q.load_state_dict({"weights" : state_dict["q_proj_weight"]})
+        self.K.load_state_dict({"weights" : state_dict["k_proj_weight"]})
+        self.V.load_state_dict({"weights" : state_dict["v_proj_weight"]})
+        self.O.load_state_dict({"weights" : state_dict["o_proj_weight"]})
