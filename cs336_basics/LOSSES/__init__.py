f9385035d1827cc06ece8e2beeba8fe26ce12168
JacekDwojakPL <dwojak.jacek@gmail.com>
2025-10-12 20:50:53 +0200

    Implement AdamW optimizer

---
+++ cs336_basics/LOSSES/__init__.py
@@ -0,0 +1,3 @@
+from .Crossentropy import crossentropy
+
+crossentropy
--- tests/adapters.py
+++ tests/adapters.py
@@ -15,6 +15,7 @@ from cs336_basics.FF import Positionwise
 from cs336_basics.ATTN import RotaryPositionEmbeddings, softmax, scaled_dot_product_attention, MultiHeadAttention
 from cs336_basics.TRANSFORMER import TransformerBlock, TransformerModel
 from cs336_basics.LOSSES import crossentropy
+from cs336_basics.OPTIM import AdamW
 
 def run_linear(
     d_in: int,
@@ -531,7 +532,7 @@ def get_adamw_cls() -> Any:
     """
     Returns a torch.optim.Optimizer that implements AdamW.
     """
-    raise NotImplementedError
+    return AdamW
 
 
 def run_get_lr_cosine_schedule(
